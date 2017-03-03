7c64537b84294fa243728d9dd7329995f6ed4e7e
cray77 <cray@soarr.com>
2017-03-03 15:23:38 -0500

    Initial commit

---
+++ www/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from '../scripts/src/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
